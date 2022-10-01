c178841aca949a152fac548a0bd5e507c5d78108
anwjrrp33 <anwjrrp33@gmail.com>
2022-10-01 15:40:25 +0900

    sequelize 적용

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+var { sequelize } = require('./models/index')
+// 기존 테이블 삭제 후 생성
+// 서버 실행시 MYSQL과 연결
+sequelize.sync({ force: true }) // 서버 실행시마다 테이블을 재생성할건지에 대한 여부
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
