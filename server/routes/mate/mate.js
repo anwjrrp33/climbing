7334fac273d0019f963d1231a15ba063b63acce0
anwjrrp33 <anwjrrp33@gmail.com>
2022-10-10 23:16:06 +0900

    feat: 커뮤니티 메이트 리스트 기능 구현

--- server/routes/mate/mate.js
+++ server/routes/mate/mate.js
@@ -1,7 +1,30 @@
 const { Mate } = require("../../models");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
 
 async function list(req, res, next) {
+    let search = !req.param("search") ? "" : req.param("search");
+    let page = !req.param("page") ? 1 : req.param("page");
+    let limit = 10;
+    let offset = 0;
 
+    if (page > 1) {
+        offset = limit * (page - 1);
+    }
+
+    const mates = await Mate.findAndCountAll({
+        where: {
+            [Op.or]: [
+                { title: { [Op.like]: '%' + search + "%" } },
+                { content: { [Op.like]: '%' + search + "%" } }
+            ]
+        },
+        order: [['id', 'DESC']],
+        offset: offset,
+        limit: limit
+    });
+
+    return res.status(200).json({ success: true, mates: mates });
 }
 
 async function register(req, res, next) {
