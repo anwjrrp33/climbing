25d2988f118c0f9d1bf77541f710d432bca92ff8
anwjrrp33 <anwjrrp33@gmail.com>
2022-10-13 18:04:15 +0900

    feat: 라우트 변경

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,11 @@
 const router = require('express').Router();
 
-
-
-
-const homeRouter = require('./home')
-const communityRouter = require('./mate')
-
+const homeRouter = require('./home');
+const mateRouter = require('./mate');
+const mateCommentRouter = require('./mateComment');
 
 router.use('/',  homeRouter); //home 화면
-router.use('/', communityRouter); // 커뮤니티
-
- 
+router.use('/', mateRouter); // 커뮤니티 - 메이트
+router.use('/', mateCommentRouter); // 커뮤니티 - 메이트 댓글
 
 module.exports = router;
