9a49509b97cfb930e2b092664d7db9c1815c83fd
joon shim <joonshim@joonui-MacBookPro.local>
2022-10-15 11:08:46 +0900

    feat: 메인 페이지 배너슬라이드 기능 구현, 커뮤니티/암장 정보 슬라이드 구현, 헤더 페이지 이동 기능 추가

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,25 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from 'pages/home';
-import CragList from 'pages/crag/List';
-import CragDetail from 'pages/crag/Detail';
-import MateList from 'pages/mate/List';
-import MateDetail from 'pages/mate/Detail';
-import NotFound from 'pages/notfound';
-import { Layout } from 'components/Layout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "pages/home";
+import CragList from "pages/crag/List";
+import CragDetail from "pages/crag/Detail";
+import MateList from "pages/mate/List";
+import MateDetail from "pages/mate/Detail";
+import NotFound from "pages/notfound";
+import Header from "components/Layout/header";
+// import { Layout } from "components/Layout";
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path='/' element={<Main />}></Route>
-          <Route path='/crag' element={<CragList />} />
-          <Route path='/crag/:cragId' element={<CragDetail />} />
-          <Route path='/mate' element={<MateList />} />
-          <Route path='/mate/:mateId' element={<MateDetail />} />
-          <Route path='/*' element={<NotFound />} />
-        </Routes>
-      </Layout>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/crag" element={<CragList />} />
+        <Route path="/crag/:cragId" element={<CragDetail />} />
+        <Route path="/mate" element={<MateList />} />
+        <Route path="/mate/:mateId" element={<MateDetail />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}', 'node_modules/flowbite-react/**/*.{js,jsx}'],
+  content: ["./src/**/*.{js,jsx}", "node_modules/flowbite-react/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require("flowbite/plugin")],
 };
