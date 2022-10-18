1b2fd58903234ec2ec8056dfb8ddefbd1b62ebec
Yena-Yun <cheryleduart@gmail.com>
2022-10-18 14:14:21 +0900

    feat: 에디터에 color picker 및 언어 설정 추가

--- client/src/pages/mate/Create.jsx
+++ client/src/pages/mate/Create.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Editor } from '@toast-ui/react-editor';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import '@toast-ui/editor/dist/i18n/ko-kr';
 
 const MateCreate = () => {
+  const [content, setContent] = useState();
   const editorRef = React.createRef();
 
-  const handleClick = () => {
-    editorRef.current.getInstance().exec('bold');
+  const handleSubmit = () => {
+    // '에디터에서 블록 지정한 텍스트를 bold 지정하라'
+    // editorRef.current.getInstance().exec('bold');
+    console.log(editorRef.current.getInstance());
+  };
+
+  const handleChange = () => {
+    console.log('handlechange 실행!');
+    // setContent();
   };
 
   return (
@@ -15,9 +27,10 @@ const MateCreate = () => {
         placeholder='등록할 내용을 적어주세요..'
         height='600px'
         initialEditType='wysiwyg'
-        useCommandShortcut={true}
         hideModeSwitch={true}
+        useCommandShortcut={true}
         autofocus={true}
+        plugins={[colorSyntax]}
         toolbarItems={[
           ['heading', 'bold', 'italic', 'strike'],
           ['ul', 'ol'],
@@ -25,8 +38,11 @@ const MateCreate = () => {
           ['table', 'image', 'link'],
         ]}
         ref={editorRef}
+        language='ko-KR'
+        // onChange={() => { }}
+        events={{ change: () => handleChange }}
       />
-      <button onClick={handleClick}>make bold</button>
+      <button onClick={handleSubmit}>등록</button>
     </>
   );
 };
