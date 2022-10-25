ac4a3a09360217d811b219097f1fb111bb83a7c6
anwjrrp33 <anwjrrp33@gmail.com>
2022-10-25 13:31:41 +0900

    feat: 미들웨어 추가

--- server/routes/mateComment/index.js
+++ server/routes/mateComment/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const MateComment = require('./mateComment');
+const middleware = require('../middleware/middleware');
 
 router.get('/mateComment/:mateId', MateComment.list);
 router.get('/mateComment/:mateId/:parentId', MateComment.parentList);
-router.post('/mateComment/:mateId', MateComment.register);
-router.put('/mateComment/:mateId/:id', MateComment.modify);
-router.delete('/mateComment/:mateId/:id', MateComment.remove);
+router.post('/mateComment/:mateId', middleware, MateComment.register);
+router.put('/mateComment/:mateId/:id', middleware, MateComment.modify);
+router.delete('/mateComment/:mateId/:id', middleware, MateComment.remove);
 
 module.exports = router;
